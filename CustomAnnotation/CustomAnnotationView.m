706cd5ac0feed6a6194ff3a2f911329a514790a5
Hiren <hiren.bhadreshwara@gmail.com>
2013-04-18 12:29:26 +0530

    Create new project
    Create custom annotation view with multiple text and images.

---
+++ CustomAnnotation/CustomAnnotationView.m
@@ -0,0 +1,145 @@
+//
+//  CustomAnnotationView.m
+//  CustomAnnotation
+//
+//  Created by http://Technopote.com on 11/04/13.
+//  Copyright (c) 2013 http://Technopote.com. All rights reserved.
+//
+
+#import "CustomAnnotationView.h"
+#import "Annotation.h"
+
+@implementation CustomAnnotationView
+
+@synthesize calloutView, flightLbl, regLbl, routeLbl, dtLbl;
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated{
+    [super setSelected:selected animated:animated];
+
+    Annotation *ann = self.annotation;
+    if(selected)
+    {
+        //Add your custom view to self...
+        flightLbl = [[UILabel alloc] initWithFrame:CGRectMake(-70, 70, 120, 20)];
+        flightLbl.backgroundColor = [UIColor clearColor];
+        flightLbl.text = ann.title;
+        flightLbl.font=[UIFont boldSystemFontOfSize:12];
+        flightLbl.shadowOffset = CGSizeMake(0, 1);
+        flightLbl.adjustsFontSizeToFitWidth = YES;
+
+
+        regLbl = [[UILabel alloc] initWithFrame:CGRectMake(-70, 90, 120, 20)];
+        regLbl.backgroundColor = [UIColor clearColor];
+        regLbl.text = ann.subtitle;
+        regLbl.font=[UIFont boldSystemFontOfSize:12];
+        regLbl.shadowOffset = CGSizeMake(0, 1);
+        regLbl.adjustsFontSizeToFitWidth = YES;
+
+
+        routeLbl = [[UILabel alloc] initWithFrame:CGRectMake(-70, 110, 120, 20)];
+        routeLbl.backgroundColor = [UIColor clearColor];
+        routeLbl.text = ann.thirdtitle;
+        routeLbl.font=[UIFont boldSystemFontOfSize:12];
+        routeLbl.shadowOffset = CGSizeMake(0, 1);
+        routeLbl.adjustsFontSizeToFitWidth = YES;
+
+
+        dtLbl = [[UILabel alloc] initWithFrame:CGRectMake(-70, 130, 120, 20)];
+        dtLbl.backgroundColor = [UIColor clearColor];
+        dtLbl.text = ann.fourthtitle;
+        dtLbl.font=[UIFont boldSystemFontOfSize:12];
+        dtLbl.shadowOffset = CGSizeMake(0, 1);
+        dtLbl.adjustsFontSizeToFitWidth = YES;
+        
+        calloutView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"box.png"]];
+
+/*        if ([ann.locationType isEqualToString:@"airport"]) {
+            calloutView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"box.png"]];
+        }
+        if ([ann.locationType isEqualToString:@"restaurant"]) {
+            calloutView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"restaurant.png"]];
+        }
+        if ([ann.locationType isEqualToString:@"shopping"]) {
+            calloutView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"shopping.png"]];
+        }
+*/
+        [calloutView setFrame:CGRectMake(-71, 18, 0, 0)];
+        [calloutView sizeToFit];
+        
+
+        flightLbl.textColor = [UIColor whiteColor];
+        regLbl.textColor = [UIColor whiteColor];
+        routeLbl.textColor = [UIColor whiteColor];
+        dtLbl.textColor = [UIColor whiteColor];
+        
+        [self animateCalloutAppearance];
+        [self addSubview:calloutView];
+        [self addSubview:flightLbl];
+        [self addSubview:regLbl];
+        [self addSubview:routeLbl];
+        [self addSubview:dtLbl];
+    }
+    else
+    {
+        //Remove your custom view...
+        [calloutView removeFromSuperview];
+        [flightLbl removeFromSuperview];
+        [regLbl removeFromSuperview];
+        [routeLbl removeFromSuperview];
+        [dtLbl removeFromSuperview];
+    }
+}
+
+- (void)didAddSubview:(UIView *)subview{
+    Annotation *ann = self.annotation;
+    if (![ann.locationType isEqualToString:@"dropped"]) {
+        if ([[[subview class] description] isEqualToString:@"UICalloutView"]) {
+            for (UIView *subsubView in subview.subviews) {
+                if ([subsubView class] == [UIImageView class]) {
+                    UIImageView *imageView = ((UIImageView *)subsubView);
+                    [imageView removeFromSuperview];
+                }else if ([subsubView class] == [UILabel class]) {
+                    UILabel *labelView = ((UILabel *)subsubView);
+                    [labelView removeFromSuperview];
+                }
+            }
+        }
+    }
+}
+
+- (void)animateCalloutAppearance {
+    CGFloat scale = 0.001f;
+    calloutView.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -50);
+    flightLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -50);
+    regLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -50);
+    routeLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -50);
+    dtLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -50);
+    [UIView animateWithDuration:0.15 delay:0 options:UIViewAnimationCurveEaseOut animations:^{
+        CGFloat scale = 1.1f;
+        calloutView.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 2);
+        flightLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 2);
+        regLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 2);
+        routeLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 2);
+        dtLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 2);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.1 delay:0 options:UIViewAnimationCurveEaseInOut animations:^{
+            CGFloat scale = 0.95;
+            calloutView.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -2);
+            flightLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -2);
+            routeLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -2);
+            regLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -2);
+            dtLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, -2);
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:0.075 delay:0 options:UIViewAnimationCurveEaseInOut animations:^{
+                CGFloat scale = 1.0;
+                calloutView.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 0);
+                flightLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 0);
+                routeLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 0);
+                regLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 0);
+                dtLbl.transform = CGAffineTransformMake(scale, 0.0f, 0.0f, scale, 0, 0);
+            } completion:nil];
+        }];
+    }];
+}
+
+@end
